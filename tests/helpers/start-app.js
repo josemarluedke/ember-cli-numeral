2fa6b6d77767f0557ec0df8ed1173c11c9763009
Josemar Luedke <josemarluedke@gmail.com>
2016-10-30 15:35:16 -0700

    Add missing semicolon

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -4,7 +4,7 @@ import config from '../../config/environment';
 
 export default function startApp(attrs) {
   let application;
-  let assign = Ember.assign || Ember.merge
+  let assign = Ember.assign || Ember.merge;
 
   // use defaults, but you can override
   let attributes = assign({}, config.APP, attrs);
